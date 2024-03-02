6738df90e2c6efa0d7844e91d4eae32421fd0658
R0bot0 <160941500+ElR0bot0@users.noreply.github.com>
2024-03-02 07:59:46 -0500

    Movimiento y animaciones

--- Assets/Characters/Player/PlayerController.cs
+++ Assets/Characters/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
     public float moveSpeed =1f;
     public ContactFilter2D movementFilter;
+    Animator animator;
     
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     Rigidbody2D rb;
@@ -16,14 +18,47 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
         if(movementInput!= Vector2.zero){
-            rb.Cast(movementInput,movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime+collisionOffset);
+            bool success = TryMove(movementInput);
+            
+            if(!success){
+                success = TryMove(new Vector2(movementInput.x, 0));
+                if(!success){
+                    success = TryMove(new Vector2(movementInput.y, 0));
+                }
+            }
+        }else{
+            animator.SetBool("Mvdwn", false);
+            animator.SetBool("Mvup", false);
         }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+            int count = rb.Cast(direction,movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime+collisionOffset);
+            if(count==0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                //Aqui defino los booleanos de la
+                if(direction.y>0){
+                    animator.SetBool("Mvdwn", false);
+                    animator.SetBool("Mvup", true);
+                }else if(direction.y<0){
+                    animator.SetBool("Mvdwn", true);
+                    animator.SetBool("Mvup", false);
+                }else{
+                    animator.SetBool("Mvdwn", false);
+                    animator.SetBool("Mvup", false);
+                }
+                return true;
+            }else{
+                return false;
+            }
     }
     void OnMove(InputValue movementValue)
     {
